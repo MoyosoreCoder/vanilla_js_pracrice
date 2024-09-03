c9775e2aaeb66f82cec9e0c635680196542c809b
Mariam <kelanimaryam1234@gmail.com>
2024-09-03 17:21:15 +0100

    javascript search engine file created, index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function handleClick(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = searchInput.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleClick);
